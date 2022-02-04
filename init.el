2d587b79c642c4fa87e96e5c5ea6a86ae32c31f1
Tanner Davies <tanner.e.davies@gmail.com>
2022-02-04 12:35:41 -0700

    Set default frame size

--- init.el
+++ init.el
@@ -1,45 +1,60 @@
+;; Set theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'naysayer t)
 
+;; Default window size
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+    (progn
+      ;; Use fewer columns for smaller windows
+      (if (> (x-display-pixel-width) 1280)
+          (add-to-list 'default-frame-alist (cons 'width 180))
+        (add-to-list 'default-frame-alist (cons 'width 100)))
+      ;; Subtract a few pixels off of total screen height
+      (add-to-list 'default-frame-alist 
+		   (cons 'height (/ (- (x-display-pixel-height) 260)
+				    (frame-char-height)))))))
+(set-frame-size-according-to-resolution)
+
+
+
+;; Some Emacs-generated stuff to trust the naysayer theme
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("399bce2ec203f474cdd3e4463863011dab044da9618b9f398785714d64e1cb1c" "d2e44214a7dc0bd5b298413ed6c3ba9719f1d96794d9de3bdf7a9808902fd098" default))
  '(linum-format " %5i ")
  '(window-divider-mode t))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(window-divider ((t (:background "gray60" :foreground "gray60")))))
+ '(window-divider ((t (:background "gray60" :foreground "gray80")))))
 
-(defun toggle-comment-on-line ()
-  "comment or uncomment current line"
-  (interactive)
-  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
-
+;; Don't poop backup files everywhere (put them in a system temp directory)
 (setq backup-directory-alist
     `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
     `((".*" ,temporary-file-directory t)))
 
+;; Don't remap C-x to C-c when using `M-x term`
 (add-hook 'term-mode-hook
    (lambda ()
-     ;; C-x is the prefix command, rather than C-c
      (term-set-escape-char ?\C-x)
      (define-key term-raw-map "\M-y" 'yank-pop)
      (define-key term-raw-map "\M-w" 'kill-ring-save)))
 
-;; Keybindings
+;; Don't show menubar or toolbar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
 
+;; Function for commenting current line
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+
+;; Keybindings
 (global-set-key (kbd "C-c C-c") 'toggle-comment-on-line)
 
-;; Language support
-
+;; Rust Language support
 (add-to-list 'load-path "/Users/tanner/.emacs.d/rust-mode-master")
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
