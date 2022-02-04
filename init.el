2d3c7082169ed3c5b5a79316a527a885e0e37cf9
Tanner Davies <tanner.e.davies@gmail.com>
2022-02-04 08:45:52 -0700

    Add keybinding for commenting (C-c C-c) and max M-x term more pleasant

--- init.el
+++ init.el
@@ -1,7 +1,3 @@
-(add-to-list 'load-path "/Users/tanner/.emacs.d/rust-mode-master")
-(autoload 'rust-mode "rust-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'naysayer t)
 
@@ -11,7 +7,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d2e44214a7dc0bd5b298413ed6c3ba9719f1d96794d9de3bdf7a9808902fd098" default))
+   '("399bce2ec203f474cdd3e4463863011dab044da9618b9f398785714d64e1cb1c" "d2e44214a7dc0bd5b298413ed6c3ba9719f1d96794d9de3bdf7a9808902fd098" default))
+ '(linum-format " %5i ")
  '(window-divider-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -29,3 +26,20 @@
     `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
     `((".*" ,temporary-file-directory t)))
+
+(add-hook 'term-mode-hook
+   (lambda ()
+     ;; C-x is the prefix command, rather than C-c
+     (term-set-escape-char ?\C-x)
+     (define-key term-raw-map "\M-y" 'yank-pop)
+     (define-key term-raw-map "\M-w" 'kill-ring-save)))
+
+;; Keybindings
+
+(global-set-key (kbd "C-c C-c") 'toggle-comment-on-line)
+
+;; Language support
+
+(add-to-list 'load-path "/Users/tanner/.emacs.d/rust-mode-master")
+(autoload 'rust-mode "rust-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
