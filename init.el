7de5d8c41f4693b9dd066a15257ff6b783aa85a5
Tanner Davies <tanner.e.davies@gmail.com>
2022-04-01 22:10:18 -0600

    Add markdown mode

--- init.el
+++ init.el
@@ -93,6 +93,16 @@
 (autoload 'swift-mode "swift-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.swift\\'" . swift-mode))
 
+;; Markdown support
+(add-to-list 'load-path "~/.emacs.d/markdown-mode-master")
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . markdown-mode))
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
 ;; In C, don't indent braces according to GNU style
 (setq c-default-style "linux"
       c-basic-offset 4)
