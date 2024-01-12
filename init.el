9b7e6f894524e6857e1c83107fa0e050718a8835
Tanner Davies <tanner.e.davies@protonmail.com>
2024-01-11 17:10:32 -0700

    Add OCaml support

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
             (setq gc-cons-threshold (* 10 1024 1024))
             (delete-other-windows)))
 
+(require 'lsp-mode)
+
 (add-hook 'after-init-hook 'global-company-mode)
 (add-hook 'c-mode-hook #'lsp-deferred)
 (add-hook 'c++-mode-hook #'lsp-deferred)
@@ -322,3 +324,21 @@ the minibuffer alone."
 (put 'narrow-to-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+
+;; OCaml
+(let ((opam-share (ignore-errors (car (process-lines "opam" "var" "share")))))
+  (when (and opam-share (file-directory-p opam-share))
+    ;; Register Merlin
+    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+    (autoload 'merlin-mode "merlin" nil t nil)
+    ;; Automatically start it in OCaml buffers
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)
+    (add-hook 'caml-mode-hook 'merlin-mode t)
+    ;; Use opam switch to lookup ocamlmerlin binary
+    (setq merlin-command 'opam)
+    ;; To easily change opam switches within a given Emacs session, you can
+    ;; install the minor mode https://github.com/ProofGeneral/opam-switch-mode
+    ;; and use one of its "OPSW" menus.
+    ))
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+
