f2fccd7f68dcf6e0785ba33586360675f2e16086
Tanner Davies <tanner.e.davies@protonmail.com>
2024-01-12 12:36:02 -0700

    Update deps

--- elpa/web-mode-17.3.14/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from web-mode.el  -*- no-byte-compile: t -*-
-(define-package "web-mode" "17.3.14" "major mode for editing web templates" '((emacs "23.1")) :commit "443a09423d23727df7efd4e60beedd3919e4800a" :maintainer '("François-Xavier Bois" . "fxbois@gmail.com") :keywords '("languages") :url "https://web-mode.org")
---
+++ elpa/web-mode-17.3.17/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from web-mode.el  -*- no-byte-compile: t -*-
+(define-package "web-mode" "17.3.17" "major mode for editing web templates" '((emacs "23.1")) :commit "82847071ce93293bdb7945db08d970f13fd883cf" :maintainer '("François-Xavier Bois" . "fxbois@gmail.com") :keywords '("languages") :url "https://web-mode.org")
