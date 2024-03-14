4c4c64a3d589b5253180119a39bc02f81cc88f49
Tanner Davies <tanner.davies@cbtravel.com>
2024-03-13 21:22:04 -0600

    Update dependencies

---
+++ elpa/flycheck-20240307.2106/flycheck-pkg.el
@@ -0,0 +1,14 @@
+(define-package "flycheck" "20240307.2106" "On-the-fly syntax checking"
+  '((emacs "26.1"))
+  :commit "79d20bf37ad90086c362dfcd8254bc94f66ed243" :authors
+  '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
+  :maintainers
+  '(("Clément Pit-Claudel" . "clement.pitclaudel@live.com"))
+  :maintainer
+  '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
+  :keywords
+  '("convenience" "languages" "tools")
+  :url "https://www.flycheck.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/web-mode-17.3.19/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from web-mode.el  -*- no-byte-compile: t -*-
-(define-package "web-mode" "17.3.19" "major mode for editing web templates" '((emacs "23.1")) :commit "10f1269ab1007ab3d5fe018c83bb82c979617bfd" :maintainer '("François-Xavier Bois" . "fxbois@gmail.com") :keywords '("languages") :url "https://web-mode.org")
